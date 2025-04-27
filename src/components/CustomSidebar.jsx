f12eee0c8354b9cc2fb3f2bd9d4abc7c1797be1d
aicha-manel <chf.aimanel@gmail.com>
2025-04-27 10:14:26 +0100

    creation dossier css

--- src/components/CustomSidebar.jsx
+++ src/components/CustomSidebar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Sidebar as ProSidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
 import { FaBars, FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
-import './CustomSidebar.css'; 
+
+import './css/CustomSidebar.css'
 import LogoProfil from './images/portrait.jpg';
 import { Link } from 'react-router-dom'; // Ajouter ce import
 
