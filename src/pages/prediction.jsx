b27c471f093467d80e46488a85f452594acc787a
aicha-manel <chf.aimanel@gmail.com>
2025-04-27 11:12:15 +0100

    ajouter code prediction

--- src/pages/prediction.jsx
+++ src/pages/prediction.jsx
@@ -6,15 +6,19 @@ import GrapheSection from '../components/predComponent/grapheSection';
 import GrapheAction from '../components/predComponent/grapheAction';
 import MethodSummary from '../components/predComponent/methodSummary';
 import PredictionResults from '../components/predComponent/predictionResults';
+import CustomSidebar from '../components/CustomSidebar';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
 const Prediction = () => {
   return (
     <div className="flex h-screen">
       {/* Sidebar fixe à gauche */}
-      <Sidebar />
+        <ProSidebarProvider>
+              <CustomSidebar />
+            </ProSidebarProvider>
 
       {/* Contenu principal prend le reste */}
-      <div className="flex-1 p-8 pt-5  overflow-auto bg-neutral-200">
+      <div className="flex-1 p-8   overflow-auto bg-neutral-200">
         <div className="flex justify-between items-center mb-6 rounded  p-3  bg-white shadow">
           <h5 className="text-lg font-bold text-gray-600 ">Prediction</h5>
           <h5 className="text-lg font-bold text-gray-600 ">Nom de l'entreprise</h5>
