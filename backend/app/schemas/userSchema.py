80f3e237a812a9b3e2473a266deb0fb327534245
bnxam <bensekhriasma@gmail.com>
2025-05-11 10:56:13 +0100

    post itedu

---
+++ backend/app/schemas/userSchema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    nom: str
+    mdp: str
+
+class UserUpdate(BaseModel):
+    nom: str | None = None
+    email: EmailStr | None = None
+    mdp: str | None = None
+
+class UserResponse(UserBase):
+    id: int
+    nom: str
+
+    class Config:
+        from_attributes = True
